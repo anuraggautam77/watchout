e019ccf2bc70582da6179535b6ee41811aa381ea
Anurag <Anuraggauutam77@gmail.com>
2018-09-26 01:04:28 +0530

    data base

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,6 +1,6 @@
 const neo4j = require('neo4j');
-  const  dbConfig = {url: 'localhost:11009/', username: 'neo4j', pass: '1234', host: 'http:'};
- // const dbConfig = {url: '35.240.213.52:4502/', username: 'neo4j', pass: 'qazplm@123', host: 'http:'};
+ // const  dbConfig = {url: 'localhost:11009/', username: 'neo4j', pass: '1234', host: 'http:'};
+  const dbConfig = {url: '35.240.213.52:4502/', username: 'neo4j', pass: 'qazplm@123', host: 'http:'};
 
  
 
